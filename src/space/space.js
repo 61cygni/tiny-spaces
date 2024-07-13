3c4a345f1d50ddb036ecf06a319ffcf79c7d1322
61cygni <>
2024-07-13 15:46:51 -0700

    Cleanup in spaces
    Use an array for all spritesheets

--- src/space/space.js
+++ src/space/space.js
@@ -90,71 +90,45 @@ class Being {
     }
 }
 
-const sheet0 = await Assets.load('sprited0.json');
-const sheet1 = await Assets.load('sprited1.json');
-const sheet2 = await Assets.load('sprited2.json');
-const sheet3 = await Assets.load('sprited3.json');
+const spritesheets = [
+    'sprited0.json',
+    'sprited1.json',
+    'sprited2.json',
+    'sprited3.json',
+    'spritedX0.json',
+    'spritedX1.json',
+    'spritedX2.json',
+    'spritedX3.json',
+    'ps2-small0.json',   
+    'ps2-small1.json',
+    'ps2-small2.json',
+    'ps1-others0.json',
+    'ps1-others1.json',
+    'ps1-others2.json',
+    'ps1-alice0.json'
+]
 
-const sheetX0 = await Assets.load('spritedX0.json');
-const sheetX1 = await Assets.load('spritedX1.json');
-const sheetX2 = await Assets.load('spritedX2.json');
-const sheetX3 = await Assets.load('spritedX3.json');
-
-const sheetsmall0 = await Assets.load('ps2-small0.json');
-const sheetsmall1 = await Assets.load('ps2-small1.json');
-const sheetsmall2 = await Assets.load('ps2-small2.json');
-
-const sheetps10    = await Assets.load('ps1-others0.json');
-const sheetps11    = await Assets.load('ps1-others1.json');
-const sheetps12    = await Assets.load('ps1-others2.json');
-const sheetps13    = await Assets.load('ps1-alice0.json');
+let beings = []
 
 const app = new PIXI.Application();
-
 app.init({ width: 640, height: 480, canvas: document.getElementById('spacecanvas') });
 
-let b1 = new Being(app, sheet0, 128,150)
-let b2 = new Being(app, sheet1, 156,150)
-let b3 = new Being(app, sheet2, 192,150)
-let b4 = new Being(app, sheet3, 228,150)
-
-let b5 = new Being(app, sheetX0, 128,96)
-let b6 = new Being(app, sheetX1, 156,96)
-let b7 = new Being(app, sheetX2, 192,96)
-let b8 = new Being(app, sheetX3, 228,96)
-
-let b9  = new Being(app, sheetsmall0, 128,192)
-let b10 = new Being(app, sheetsmall1, 156,192)
-let b11 = new Being(app, sheetsmall2, 192,192)
-
-let b12 = new Being(app, sheetps10, 128,224)
-let b13 = new Being(app, sheetps11, 156,224)
-let b14 = new Being(app, sheetps12, 192,224)
-let b15 = new Being(app, sheetps13, 228,224)
+for(let i = 0; i < spritesheets.length; i++){
+    const sheet = await Assets.load(spritesheets[i]);
+    let being = new Being(app, sheet, (i*32)%256, i/256);
+    beings.push(being);
+}
 
 // Add a ticker callback to move the sprite back and forth
 let elapsed = 0.0;
-
 const ticker = new Ticker();
 
 ticker.stop();
 ticker.add((deltaTime) => {
   elapsed += ticker.deltaTime;
-  b1.tick();
-  b2.tick();
-  b3.tick();
-  b4.tick();
-  b5.tick();
-  b6.tick();
-  b7.tick();
-  b8.tick();
-  b9.tick();
-  b10.tick();
-  b11.tick();
-  b12.tick();
-  b13.tick();
-  b14.tick();
-  b15.tick();
+  for(let i = 0; i < beings.length; i++){
+    beings[i].tick();
+  }
 });
 
 ticker.start();
