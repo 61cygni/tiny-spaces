7673bcf0ad0f0487f0b14228cc30cb5708972ddc
61cygni <>
2024-07-21 00:34:14 -0700

    Refactor spaces and keyboard support for Alice
    Slowly moving code to a usable app ..

---
+++ src/space/level.js
@@ -0,0 +1,174 @@
+import * as PIXI from 'pixi.js'
+import { sound } from '@pixi/sound';
+
+import { g_ctx }  from  '../shared/lecontext.js' // global context
+import * as CONFIG from '../shared/leconfig.js' 
+
+export class LevelContext {
+
+    constructor(app, mod) {
+        this.app = app
+        this.container = new PIXI.Container();
+        this.tiledimx = mod.tiledimx
+        this.tiledimy = mod.tiledimy
+        this.screenxtiles = mod.screenxtiles
+        this.screenytiles = mod.screenytiles
+        this.tilesetpxw = mod.tilesetpxw;
+        this.tilesetpxh = mod.tilesetpxh;
+        this.bgtiles = mod.bgtiles
+        this.objmap  = mod.objmap
+        this.loadFromMapFile();
+        this.app.stage.addChild(this.container)
+
+        sound.add('ps1-town', '../music/ps1-town.mp3');
+        sound.loop = true;
+        this.sound = sound;
+    }
+
+    loadFromMapFile(mod) {
+
+        for (let i = 0; i < this.bgtiles.length; i++) {
+            let tiles = this.bgtiles[i];
+            console.log("Blitting tiles for layer "+i);
+            for (let x = 0; x < tiles.length; x++) {
+                for (let y = 0; y < tiles[0].length; y++) {
+                    if (tiles[x][y] != -1) {
+                        this.addTileLevelCoords(x, y, this.tiledimx, tiles[x][y]);
+                    }
+                }
+            }
+        }
+        for (let i = 0; i < this.objmap.length; i++) {
+            let tiles = this.objmap[i];
+            console.log("Blitting tiles for layer "+i);
+            for (let x = 0; x < tiles.length; x++) {
+                for (let y = 0; y < tiles[0].length; y++) {
+                    if (tiles[x][y] != -1) {
+                        this.addTileLevelCoords(x, y, this.tiledimx, tiles[x][y]);
+                    }
+                }
+            }
+        }
+    }
+
+    addTileLevelCoords(x, y, dim, index) {
+        return this.addTileLevelPx(x * dim, y * dim, index);
+    }
+
+    tileset_coords_from_index(index) {
+        let x = index % (g_ctx.tilesettilew);
+        let y = Math.floor(index / (g_ctx.tilesettilew));
+        // console.log("tilesettilewidth: ",g_ctx.tilesettilew);
+        // console.log("tileset_coords_from_index tile coords: ",index,x,y);
+        return [x, y];
+    }
+
+    tileset_px_from_index(index) {
+        let ret = this.tileset_coords_from_index(index);
+        return [ret[0] * (g_ctx.tiledimx + CONFIG.tilesetpadding), ret[1] * (g_ctx.tiledimx + CONFIG.tilesetpadding)];
+    }
+
+
+    sprite_from_px(x, y) {
+        const bt = PIXI.Texture.from(g_ctx.tilesetpath, {
+            scaleMode: PIXI.NEAREST,
+        });
+        let w = g_ctx.tiledimx;
+        let h = g_ctx.tiledimy;
+        if(x + w > g_ctx.tilesetpxw) {
+            console.log("sprite_from_px: Warning, texture overrun, truncating");
+            w = g_ctx.tilesetpxw - x;
+        }
+        if(y + h > g_ctx.tilesetpxh) {
+            console.log("sprite_from_px: Warning, texture overrun, truncating");
+            y = g_ctx.tilesetpxh - h;
+        }
+        const crop = new PIXI.Rectangle(x, y, w, h);
+        const trim = new PIXI.Rectangle(0, 0, 256, 256);
+        let texture = new PIXI.Texture({source : bt,
+            frame: crop,
+        });
+    
+        let spr =  new PIXI.Sprite(texture);
+        return spr;
+    }
+
+    // add tile of tileset "index" to Level at location x,y
+    addTileLevelPx(x, y, index) {
+
+        let xPx = x;
+        let yPx = y;
+
+        //TODO
+        let pxloc = this.tileset_px_from_index(index);
+        let ctile = this.sprite_from_px(pxloc[0], pxloc[1]);
+        ctile.index = index;
+
+         // snap to grid
+         const dx = g_ctx.tiledimx;
+         const dy = g_ctx.tiledimy;
+ 
+         ctile.x  = Math.floor(xPx / dx) * dx; 
+         ctile.y  = Math.floor(yPx / dy) * dy;
+
+         this.container.addChild(ctile);
+    }
+
+} // class LevelContext
+
+function loadMapFromModuleFinish(mod, app, callme) {
+
+    let level = new LevelContext(app, mod);
+
+    let rrect = new PIXI.Graphics();
+    rrect.roundRect(16, 8, 128, 64, 10);
+    rrect.setStrokeStyle(2, 0xffd900, 1);
+    rrect.fill(0x0) 
+        .stroke({width:2, color:'white'});
+    app.stage.addChild(rrect);
+    callme(level);
+}
+
+async function initTilesSync(callme) {
+
+    console.log("initTileSync: "+g_ctx.tilesetpath);
+    const texture = await PIXI.Assets.load(g_ctx.tilesetpath);
+
+    if (texture.valid) {
+        console.log("Texture already valid");
+        callme();
+        return;
+    }
+
+    console.log("Loading texture ", g_ctx.tilesetpath);
+    // size of g_ctx.tileset in px
+    g_ctx.tilesetpxw = texture.width;
+    g_ctx.tilesetpxh = texture.height;
+    console.log("Texture size w:", g_ctx.tilesetpxw, "h:", g_ctx.tilesetpxh);
+    // size of g_ctx.tileset in tiles
+    let tileandpad = g_ctx.tiledimx + CONFIG.tilesetpadding;
+    let numtilesandpadw = Math.floor(g_ctx.tilesetpxw / tileandpad);
+    g_ctx.tilesettilew = numtilesandpadw + Math.floor((g_ctx.tilesetpxw - (numtilesandpadw * tileandpad)) / g_ctx.tiledimx);
+    let numtilesandpadh = Math.floor(g_ctx.tilesetpxh / tileandpad);
+    g_ctx.tilesettileh = numtilesandpadh + Math.floor((g_ctx.tilesetpxh - (numtilesandpadh * tileandpad)) / g_ctx.tiledimx);
+    console.log("Number of x tiles ", g_ctx.tilesettilew, " y tiles ", g_ctx.tilesettileh);
+
+    callme();
+}
+
+
+function loadMapFromModule(mod, app, callme) {
+    console.log(mod);
+    g_ctx.tilesetpath = mod.tilesetpath;
+    g_ctx.tiledimx = mod.tiledimx;
+    g_ctx.tiledimy = mod.tiledimy;
+
+    initTilesSync(loadMapFromModuleFinish.bind(null, mod, app, callme));
+}
+
+export function load(app, filename, callme) {
+    // level loading
+    let mod = import("../maps/ps1-camineet.js").then((mod) => {
+        loadMapFromModule(mod, app, callme);
+    });
+}
--- src/space/space.js
+++ src/space/space.js
@@ -1,7 +1,9 @@
 import { Ticker } from '@pixi/ticker';
 import { Assets } from 'pixi.js';
 import * as PIXI from 'pixi.js'
-import { sound } from '@pixi/sound';
+
+import * as LEVEL from './level.js'
+
 
 import { g_ctx }  from  '../shared/lecontext.js' // global context
 import * as CONFIG from '../shared/leconfig.js' 
@@ -9,10 +11,65 @@ import * as UTIL from   '../shared/eutils.js'
 
 const Dir = {};
 
-Dir[Dir[0] = 'UP'] = 0;
-Dir[Dir[1] = 'DOWN'] = 1;
-Dir[Dir[2] = 'LEFT'] = 2;
-Dir[Dir[3] = 'RIGHT'] = 3;
+Dir[Dir[0] = 'UP']    = 1;
+Dir[Dir[1] = 'DOWN']  = 2;
+Dir[Dir[2] = 'LEFT']  = 4;
+Dir[Dir[4] = 'RIGHT'] = 8;
+
+let mtoggle = false;
+
+let level = null;
+
+window.addEventListener(
+    "keydown", (event) => {
+
+        if (event.code == "KeyW" || event.code == 'ArrowUp') {
+            beings[0].goDir('UP');
+        }
+        else if (event.code == 'KeyS' || event.code == 'ArrowDown') {
+            // DOWN
+            beings[0].goDir('DOWN');
+        }
+        else if (event.code == 'KeyD' || event.code == 'ArrowRight') {
+            // Right 
+            beings[0].goDir('RIGHT');
+        }
+        else if (event.code == 'KeyA' || event.code == 'ArrowLeft') {
+            // Left 
+            beings[0].goDir('LEFT');
+        }
+        else if (event.code == 'KeyM'){
+            mtoggle = !mtoggle;
+            if(mtoggle){
+                level.sound.play('ps1-town');
+            }else{
+                level.sound.stop('ps1-town');
+            }
+        } 
+    }
+);
+
+window.addEventListener(
+    "keyup", (event) => {
+
+        if (event.code == "KeyW" || event.code == 'ArrowUp') {
+            beings[0].stopDir('UP');
+        }
+        else if (event.code == 'KeyS' || event.code == 'ArrowDown') {
+            // DOWN
+            beings[0].stopDir('DOWN');
+        }
+        else if (event.code == 'KeyD' || event.code == 'ArrowRight') {
+            // Right 
+            beings[0].stopDir('RIGHT');
+        }
+        else if (event.code == 'KeyA' || event.code == 'ArrowLeft') {
+            // Left 
+            beings[0].stopDir('LEFT');
+        }
+    }
+);
+
 
 function getRandomInt(max) {
     return Math.floor(Math.random() * max);
@@ -20,9 +77,10 @@ function getRandomInt(max) {
 
 class Being {
 
-    constructor(app, spritesheet, x, y) {
+    constructor(app, spritesheet, level, x, y) {
         this.app = app;
         this.sheet = spritesheet;
+        this.level = level;
         this.sprites = {};
         this.sprites['DOWN'] = new PIXI.AnimatedSprite(this.sheet.animations.row0);
         this.sprites['UP']   = new PIXI.AnimatedSprite(this.sheet.animations.row1);
@@ -37,10 +95,10 @@ class Being {
 
         this.x = x; 
         this.y = y; 
-        this.steps = 100;
         this.pausetime = Math.floor(Math.random() * 5);;
         this.pausecountdown = this.pausetime;
 
+        this.moving = 0;
         this.direction = 'DOWN'; 
         this.curanim = this.sprites['DOWN'];
         this.curanim.animationSpeed = 0.1666;
@@ -50,210 +108,133 @@ class Being {
         app.stage.addChild(this.curanim);
     }
 
-    timeToMove(){
-        let ret = this.pausecountdown--;
-        if(ret <= 0){
-            this.pausecountdown = this.pausetime;
-            return true;
+    arrive() {
+        this.curanim = this.sprites[this.direction];
+        this.curanim.x = this.x
+        this.curanim.y = this.y
+        this.curanim.animationSpeed = 0.1666;
+        this.curanim.stop();
+        this.app.stage.addChild(this.curanim);
+    }
+
+    goDir(dir) {
+        if(this.direction != dir){
+            this.direction = dir;
+            this.moving |= Dir[dir];
+            this.app.stage.removeChild(this.curanim)
+            this.curanim.stop()
+            this.x = this.curanim.x
+            this.y = this.curanim.y
+            this.curanim = this.sprites[dir];
+            this.curanim.animationSpeed = 0.1666;
+            this.curanim.x = this.x
+            this.curanim.y = this.y
+            this.curanim.play();
+            this.app.stage.addChild(this.curanim);
+        } 
+
+        else if(!this.curanim.playing){
+            this.curanim.play();
         }
-        return false;
+
+
+    }
+
+    stopDir(dir) {
+            this.moving &= ~Dir[dir]; // bitmask
+            if(this.moving == 0){
+                this.curanim.stop();
+            }
+    }
+
+    timeToMove(){
+        return true;
+
+        // FIXME
+        // let ret = this.pausecountdown--;
+        // if(ret <= 0){
+        //     this.pausecountdown = this.pausetime;
+        //     return true;
+        // }
+        // return false;
+    }
+
+    isBlocked(x, y){
+        if(!this.level){
+            return;
+        }
+        let coordsx = Math.floor(x / 16);
+        let coordsy = Math.floor(y / 16);
+        let ret = this.level.objmap[0][coordsx][coordsy] != -1;
+        if(ret){
+            console.log("blocked "+coordsx+" : "+coordsy);
+        }
+        return ret;
     }
 
     tick(){
-        if (this.steps && this.curanim.x < 624 && this.curanim.x > -1 && this.curanim.y < 448 && this.curanim.y > -1) {
+        if (this.moving && this.curanim.x < 624 && this.curanim.x > -1 && this.curanim.y < 448 && this.curanim.y > -1) {
             if (this.direction == 'RIGHT') {
                 if (this.timeToMove()) {
-                    this.curanim.x = this.curanim.x + 1;
+                    if(this.isBlocked(this.curanim.x + 16, this.curanim.y + 16)){
+                    } else {
+                        this.curanim.x = this.curanim.x + 1;
+                    }
                 }
             }
             else if (this.direction == 'LEFT') {
                 if (this.timeToMove()) {
-                    this.curanim.x = this.curanim.x - 1;
+                    if(this.isBlocked(this.curanim.x - 1, this.curanim.y + 16)){
+                    } else {
+                        this.curanim.x = this.curanim.x - 1;
+                    }
                 }
             }
             else if (this.direction == 'UP') {
                 if (this.timeToMove()) {
-                    this.curanim.y = this.curanim.y - 1;
+                    if(this.isBlocked(this.curanim.x, this.curanim.y +15)){
+                    } else {
+                        this.curanim.y = this.curanim.y - 1;
+                    }
                 }
             }
             else if (this.direction == 'DOWN') {
                 if (this.timeToMove()) {
-                    this.curanim.y = this.curanim.y + 1;
-                }
-            }
-            this.steps -= 1;
-        }else{
-            const rdir = getRandomInt(4);
-            
-            this.direction = Dir[rdir];
-
-            this.app.stage.removeChild(this.curanim)
-            this.curanim.stop()
-            if(this.curanim.x < 0){
-                this.curanim.x = 0;
-            }
-            if(this.curanim.x > 623){
-                this.curanim.x = 623;
-            }
-            if(this.curanim.y < 0){
-                this.curanim.y = 0;
-            }
-            if(this.curanim.y > 447){
-                this.curanim.y = 447;
-            }
-            this.x = this.curanim.x
-            this.y = this.curanim.y
-            this.curanim = this.sprites[this.direction];
-            this.curanim.x = this.x
-            this.curanim.y = this.y
-            this.curanim.animationSpeed = 0.1666;
-            this.curanim.play();
-            this.app.stage.addChild(this.curanim);
-            this.steps = 100;
-        }
-    }
-}
-
-async function initTilesSync(callme) {
-
-    console.log("initTileSync: "+g_ctx.tilesetpath);
-    const texture = await PIXI.Assets.load(g_ctx.tilesetpath);
-
-    if (texture.valid) {
-        console.log("Texture already valid");
-        callme();
-        return;
-    }
-
-    console.log("Loading texture ", g_ctx.tilesetpath);
-    // size of g_ctx.tileset in px
-    g_ctx.tilesetpxw = texture.width;
-    g_ctx.tilesetpxh = texture.height;
-    console.log("Texture size w:", g_ctx.tilesetpxw, "h:", g_ctx.tilesetpxh);
-    // size of g_ctx.tileset in tiles
-    let tileandpad = g_ctx.tiledimx + CONFIG.tilesetpadding;
-    let numtilesandpadw = Math.floor(g_ctx.tilesetpxw / tileandpad);
-    g_ctx.tilesettilew = numtilesandpadw + Math.floor((g_ctx.tilesetpxw - (numtilesandpadw * tileandpad)) / g_ctx.tiledimx);
-    let numtilesandpadh = Math.floor(g_ctx.tilesetpxh / tileandpad);
-    g_ctx.tilesettileh = numtilesandpadh + Math.floor((g_ctx.tilesetpxh - (numtilesandpadh * tileandpad)) / g_ctx.tiledimx);
-    console.log("Number of x tiles ", g_ctx.tilesettilew, " y tiles ", g_ctx.tilesettileh);
-
-    callme();
-}
-
-class LevelContext {
-
-    constructor(mod) {
-        this.container = new PIXI.Container();
-        this.tiledimx = mod.tiledimx
-        this.tiledimy = mod.tiledimy
-        this.screenxtiles = mod.screenxtiles
-        this.screenytiles = mod.screenytiles
-        this.tilesetpxw = mod.tilesetpxw;
-        this.tilesetpxh = mod.tilesetpxh;
-        this.bgtiles = mod.bgtiles.concat(mod.objmap)
-        this.loadFromMapFile();
-        app.stage.addChild(this.container)
-
-        sound.add('my-sound', '../music/ps1-town.mp3');
-        sound.loop = true;
-        sound.play('my-sound');
-    }
-
-    loadFromMapFile(mod) {
-
-        for (let i = 0; i < this.bgtiles.length; i++) {
-            let tiles = this.bgtiles[i];
-            console.log("Blitting tiles for layer "+i);
-            for (let x = 0; x < tiles.length; x++) {
-                for (let y = 0; y < tiles[0].length; y++) {
-                    if (tiles[x][y] != -1) {
-                        this.addTileLevelCoords(x, y, this.tiledimx, tiles[x][y]);
+                    if(this.isBlocked(this.curanim.x, this.curanim.y + 32)){
+                    } else {
+                        this.curanim.y = this.curanim.y + 1;
                     }
                 }
             }
+        }else{
+            // const rdir = getRandomInt(4);
+            
+            // this.direction = Dir[rdir];
+
+            // this.app.stage.removeChild(this.curanim)
+            // this.curanim.stop()
+            // if(this.curanim.x < 0){
+            //     this.curanim.x = 0;
+            // }
+            // if(this.curanim.x > 623){
+            //     this.curanim.x = 623;
+            // }
+            // if(this.curanim.y < 0){
+            //     this.curanim.y = 0;
+            // }
+            // if(this.curanim.y > 447){
+            //     this.curanim.y = 447;
+            // }
+            // this.x = this.curanim.x
+            // this.y = this.curanim.y
+            // this.curanim = this.sprites[this.direction];
+            // this.curanim.x = this.x
+            // this.curanim.y = this.y
+            // this.curanim.animationSpeed = 0.1666;
+            // this.curanim.play();
+            // this.app.stage.addChild(this.curanim);
         }
     }
-
-    addTileLevelCoords(x, y, dim, index) {
-        return this.addTileLevelPx(x * dim, y * dim, index);
-    }
-
-    tileset_coords_from_index(index) {
-        let x = index % (g_ctx.tilesettilew);
-        let y = Math.floor(index / (g_ctx.tilesettilew));
-        // console.log("tilesettilewidth: ",g_ctx.tilesettilew);
-        // console.log("tileset_coords_from_index tile coords: ",index,x,y);
-        return [x, y];
-    }
-
-    tileset_px_from_index(index) {
-        let ret = this.tileset_coords_from_index(index);
-        return [ret[0] * (g_ctx.tiledimx + CONFIG.tilesetpadding), ret[1] * (g_ctx.tiledimx + CONFIG.tilesetpadding)];
-    }
-
-
-    sprite_from_px(x, y) {
-        const bt = PIXI.Texture.from(g_ctx.tilesetpath, {
-            scaleMode: PIXI.NEAREST,
-        });
-        let w = g_ctx.tiledimx;
-        let h = g_ctx.tiledimy;
-        if(x + w > g_ctx.tilesetpxw) {
-            console.log("sprite_from_px: Warning, texture overrun, truncating");
-            w = g_ctx.tilesetpxw - x;
-        }
-        if(y + h > g_ctx.tilesetpxh) {
-            console.log("sprite_from_px: Warning, texture overrun, truncating");
-            y = g_ctx.tilesetpxh - h;
-        }
-        const crop = new PIXI.Rectangle(x, y, w, h);
-        const trim = new PIXI.Rectangle(0, 0, 256, 256);
-        let texture = new PIXI.Texture({source : bt,
-            frame: crop,
-        });
-    
-        let spr =  new PIXI.Sprite(texture);
-        return spr;
-    }
-
-    // add tile of tileset "index" to Level at location x,y
-    addTileLevelPx(x, y, index) {
-
-        let xPx = x;
-        let yPx = y;
-
-        //TODO
-        let pxloc = this.tileset_px_from_index(index);
-        let ctile = this.sprite_from_px(pxloc[0], pxloc[1]);
-        ctile.index = index;
-
-         // snap to grid
-         const dx = g_ctx.tiledimx;
-         const dy = g_ctx.tiledimy;
- 
-         ctile.x  = Math.floor(xPx / dx) * dx; 
-         ctile.y  = Math.floor(yPx / dy) * dy;
-
-         this.container.addChild(ctile);
-    }
-
-
-}
-
-function loadMapFromModuleFinish(mod) {
-
-    let level = new LevelContext(mod);
-    // loadAnimatedSpritesFromModule(mod);
-}
-
-function loadMapFromModule(mod) {
-    console.log(mod);
-    g_ctx.tilesetpath = mod.tilesetpath;
-    g_ctx.tiledimx = mod.tiledimx;
-    g_ctx.tiledimy = mod.tiledimy;
-
-    initTilesSync(loadMapFromModuleFinish.bind(null, mod));
 }
 
 const spritesheets = [
@@ -268,9 +249,9 @@ const spritesheets = [
     // 'spritesheets/ps2-small0.json',   
     // 'spritesheets/ps2-small1.json',
     // 'spritesheets/ps2-small2.json',
-    'spritesheets/ps1-others0.json',
-    'spritesheets/ps1-others1.json',
-    'spritesheets/ps1-others2.json',
+    // 'spritesheets/ps1-others0.json',
+    // 'spritesheets/ps1-others1.json',
+    // 'spritesheets/ps1-others2.json',
     'spritesheets/ps1-alice0.json'
     //'spritesheets/ys0.json',
     //'spritesheets/ys1.json',
@@ -285,27 +266,33 @@ app.init({ width: 640, height: 480, canvas: document.getElementById('spacecanvas
 
 for(let i = 0; i < spritesheets.length; i++){
     const sheet = await Assets.load(spritesheets[i]);
-    let being = new Being(app, sheet, (i%6)*32, Math.floor(i/6) * 64);
+    // let being = new Being(app, sheet, (i%6)*32, Math.floor(i/6) * 64);
+    let being = new Being(app, sheet, null, 276, 236); 
     beings.push(being);
 }
 
-// level loading
-let mod = import("../maps/ps1-camineet.js").then((mod) => {
-    loadMapFromModule(mod);
-});
+function init(inlevel) {
+    level = inlevel;
 
+    for (let i = 0; i < beings.length; i++) {
+        beings[i].level = level;
+        beings[i].arrive();
+    }
 
-// Add a ticker callback to move the sprite back and forth
-let elapsed = 0.0;
-const ticker = new Ticker();
+    // Add a ticker callback to move the sprite back and forth
+    let elapsed = 0.0;
+    const ticker = new Ticker();
 
-ticker.stop();
-ticker.add((deltaTime) => {
-  elapsed += ticker.deltaTime;
-  for(let i = 0; i < beings.length; i++){
-    beings[i].tick();
-  }
-});
+    ticker.stop();
+    ticker.add((deltaTime) => {
+        elapsed += ticker.deltaTime;
+        for (let i = 0; i < beings.length; i++) {
+            beings[i].tick();
+        }
+    });
 
-ticker.speed = .2;
-ticker.start();
+    ticker.speed = .2;
+    ticker.start();
+}
+
+LEVEL.load(app, "../maps/ps1-camineet.js", init);
