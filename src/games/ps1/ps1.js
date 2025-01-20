fd2772d1186a278cbe8c351fe24e569815f11136
61cygni <>
2025-01-20 14:47:01 -0800

    Add main loop logic to spaced
    Make it easier to start a game.

--- src/games/ps1/ps1.js
+++ src/games/ps1/ps1.js
@@ -1,61 +1,32 @@
-import { Ticker } from '@pixi/ticker';
+// Phantasy Star 1
+//
+// This is a simple, partial port of the original game to spaced 
+//
+// The original game is copyrighted by Sega, and is used here for educational purposes only
+//
+// You can play the original game here: https://www.retrogames.cz/play_191-SegaMS.php
+
+
 import * as PIXI from 'pixi.js'
 
-import * as LEVEL  from '@spaced/level.js';
-import * as GAME   from '@spaced/gameevents.js';
-import * as KEY    from '@spaced/keyevents.js';
+import * as SPACED from '@spaced/mainloop.js';
 
 import * as ALIS  from './alis.js';
 import * as TITLE from './title.js';
 import * as CAM   from './camineet.js';
 import * as PALMA from './palma.js';
 
-
-
 // Pixi init
 const app = new PIXI.Application();
 app.init({ width: 640, height: 480, canvas: document.getElementById('spacecanvas') });
 
-// function get_input(target){
-//     console.log(target.value);
-//     target.value = "";
-// }
-// const input = document.getElementById('myInput');
-// function register_text_input() {
-// 
-//     input.addEventListener('focus', () => {
-//         KEY.set_text_input_focus(true);
-//         console.log("FOCUS!");
-//         input.parentNode.classList.add('focused');
-//     });
-// 
-//     input.addEventListener('blur', () => {
-//         console.log("NOT FOCUS!");
-//         KEY.set_text_input_focus(false);
-//         if (!input.value) {
-//             input.parentNode.classList.remove('focused');
-//         }
-//     });
-// 
-//     KEY.register_input_handler(get_input);
-// }
-
 // Alis  (main character)
 let Alis = await ALIS.getInstance(app); 
-
-// utility functions 
-function getLevelName(tag){
-    return tag.split('-')[0];
-}
-function getLabelName(tag){
-    return tag.split('-')[1];
-}
 
 // Level-Label to start the game on 
 //const startlocation = "Camineet-start1";
 //const startlocation = "Title-start1";
 const startlocation = "Palma-start1";
-
 
 //  all levels to preload
 const levels = [
@@ -64,92 +35,5 @@ const levels = [
     PALMA.Instance,
 ];
 
-let levelmap = new Map();
-
-function alisEnterLevel(location, gameevents) {
-    let levelname = getLevelName(location);
-    let labelname = getLabelName(location);
-
-    console.log("Entering level " + levelname + ' : ' + labelname);
-
-    let level = levelmap.get(levelname);
-
-    if (level.details.type == "level") {
-        Alis.level = level;
-        gameevents.reset(level, Alis);
-        level.arrive();
-
-        let start = level.coordsdict.get(labelname);
-        console.log("Alis start " + start[0] + " : " + start[1]);
-        Alis.arrive(start[0] * level.tiledimx, start[1] * level.tiledimy - 14);
-    }else{
-        // Splash screens don't need Alis 
-        gameevents.reset(level, null);
-        level.arrive();
-    }
-
-    // Fade in 
-    gameevents.add_to_tick_event_queue(new GAME.FadeIn(gameevents));
-
-    // call level initialization when entering
-    level.details.initonenter(gameevents);
-
-    return level;
-}
-
-function init() {
-
-    // game event harness
-    let gameevents = new GAME.GameEvents(Alis);
-    // initialize keyboard handler
-    KEY.init(gameevents, Alis);
-    //register_text_input();
-
-    const ticker = new Ticker();
-
-    // Alis enters first level
-    let level = alisEnterLevel(startlocation, gameevents);
-
-    // Main loop
-    ticker.add((delta) => {
-        Alis.tick(delta);
-        let nextlevel = gameevents.tick(delta);
-        if(nextlevel != null){
-
-            if(Alis.hp <= 0){
-                console.log("Alis is dead. Going to title screen");
-                nextlevel = "Title-start1";
-            }
-
-            ticker.stop();
-            console.log("Leaving "+getLevelName(nextlevel));
-            Alis.leave();
-            level.leave();
-
-            level = alisEnterLevel(nextlevel, gameevents);
-            ticker.start();
-        }
-    });
-
-    ticker.speed = .2;
-    ticker.start();
-}
-
-//  Preload all levels this requires a bunch of async calls, so kick off all of them
-//    and then wait for them all to complete before running the main loop 
-
-let promises = levels.map((leveldetails) =>{
-  return LEVEL.loadSync(app, leveldetails);
-}
-);
-
-Promise.all(promises).then((levels) => {
-    levels.map((alevel) => {
-        console.log("Loaded level: "+alevel.name);
-        levelmap.set(alevel.name, alevel)
-    });
-
-    // initial entry to main loop
-    init();
-}
-);
+await SPACED.initAndLoadLevels(app, Alis, levels);
+SPACED.initMainLoop(startlocation);
---
+++ src/spaced/mainloop.js
@@ -0,0 +1,152 @@
+// mainloop.js
+//
+// Helper functions for the main loop of any game. It handles:
+// - Loading all assets (images, sounds, etc)
+// - Loading all levels
+// - loading all sprites
+//
+// I also maintains a map of names to levels. And a basic
+// game loop handling transitions between levels
+
+import { Ticker } from '@pixi/ticker';
+
+import * as LEVEL  from '@spaced/level.js';
+import * as GAME   from '@spaced/gameevents.js';
+import * as KEY    from '@spaced/keyevents.js';
+
+
+// Main character
+let mainchar = null;
+// map of level name to level
+let levelmap = new Map();
+
+// OK these are annoyingly magic. But the current convention is to use
+// lavelname-labelname when descring an entry point to a level
+
+export function getLevelName(tag){
+    return tag.split('-')[0];
+}
+export function getLabelName(tag){
+    return tag.split('-')[1];
+}
+
+
+function maincharEnterLevel(location, gameevents) {
+    let levelname = getLevelName(location);
+    let labelname = getLabelName(location);
+
+    console.log("Entering level " + levelname + ' : ' + labelname);
+
+    let level = levelmap.get(levelname);
+
+    if (level.details.type == "level") {
+        mainchar.level = level;
+        gameevents.reset(level, mainchar);
+        level.arrive();
+
+        let start = level.coordsdict.get(labelname);
+        console.log("mainchar start " + start[0] + " : " + start[1]);
+        mainchar.arrive(start[0] * level.tiledimx, start[1] * level.tiledimy - 14);
+    }else{
+        // Splash screens don't need mainchar 
+        gameevents.reset(level, null);
+        level.arrive();
+    }
+
+    // Fade in 
+    gameevents.add_to_tick_event_queue(new GAME.FadeIn(gameevents));
+
+    // call level initialization when entering
+    level.details.initonenter(gameevents);
+
+    return level;
+}
+
+// Call this first with main character of class Being
+// and an array of levels.
+
+export async function initAndLoadLevels(app, mainbeing, levels) {
+    mainchar = mainbeing;
+
+    //  Preload all levels this requires a bunch of async calls, so kick off all of them
+    //    and then wait for them all to complete before running the main loop 
+
+    let promises = levels.map((leveldetails) => {
+        return LEVEL.loadSync(app, leveldetails);
+    }
+    );
+
+    await Promise.all(promises).then((levels) => {
+        levels.map((alevel) => {
+            console.log("Loaded level: " + alevel.name);
+            levelmap.set(alevel.name, alevel)
+        });
+    }
+    );
+
+}
+
+// Call this to start the main loop. Must be called after initAndLoadLevels
+// finishes.
+export function initMainLoop(startlocation) {
+    // game event harness
+    let gameevents = new GAME.GameEvents(mainchar);
+
+    // initialize keyboard handler
+    KEY.init(gameevents, mainchar);
+    //register_text_input();
+
+    const ticker = new Ticker();
+
+    // mainchar enters first level
+    let level = maincharEnterLevel(startlocation, gameevents);
+
+    // Main loop
+    ticker.add((delta) => {
+        mainchar.tick(delta);
+        let nextlevel = gameevents.tick(delta);
+        if (nextlevel != null) {
+
+            if (mainchar.hp <= 0) {
+                console.log("mainchar is dead. Going to title screen");
+                nextlevel = "Title-start1";
+            }
+
+            ticker.stop();
+            console.log("Leaving " + getLevelName(nextlevel));
+            mainchar.leave();
+            level.leave();
+
+            level = maincharEnterLevel(nextlevel, gameevents);
+            ticker.start();
+        }
+    });
+
+    ticker.speed = .2;
+    ticker.start();
+}
+
+// Placeholder for using HTML input field for text input
+// function get_input(target){
+//     console.log(target.value);
+//     target.value = "";
+// }
+// const input = document.getElementById('myInput');
+// function register_text_input() {
+// 
+//     input.addEventListener('focus', () => {
+//         KEY.set_text_input_focus(true);
+//         console.log("FOCUS!");
+//         input.parentNode.classList.add('focused');
+//     });
+// 
+//     input.addEventListener('blur', () => {
+//         console.log("NOT FOCUS!");
+//         KEY.set_text_input_focus(false);
+//         if (!input.value) {
+//             input.parentNode.classList.remove('focused');
+//         }
+//     });
+// 
+//     KEY.register_input_handler(get_input);
+// }
