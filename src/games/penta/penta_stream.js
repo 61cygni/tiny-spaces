ebd6b1622785c0826cfe1f987c3ac30cef209ef0
61cygni <>
2025-02-14 11:36:12 -0800

    have streaming working

---
+++ src/games/penta/penta_stream.js
@@ -0,0 +1,71 @@
+// --
+// penta_stream.js
+// 
+// Handled stream responses from Braintrust for Penta including all object and action parsing
+// 
+// --
+
+import * as BT  from '@spaced/bt.js';
+
+
+export async function invoke_prompt_input_stream(impl, slug, sysinput){
+
+    // const sysinput = {
+    //     history: this.history,
+    //     visits: this.visits,
+    //     msg: val,
+    // };
+
+    // let usrinput = this.user_input();
+    let usrinput = {};
+    let promptinput = {...sysinput, ...usrinput};
+
+    console.log("Prompt: "+promptinput);
+
+    let result = await BT.asyncbtStream(slug, promptinput);
+    let preamble = "";
+    let session = "";
+
+    let afterstopword = false;
+    if (impl.is_chatting) {
+        impl.gameevents.dialog_stream(preamble, 'character', {character: impl.chatting_with_villager});
+        for await (const chunk of result) {
+            if (!impl.is_chatting){
+                break;
+            }
+
+            if(chunk.data.includes("###")){
+                impl.gameevents.dialog_stream(chunk.data.split("###")[0], 'character', {character: impl.chatting_with_villager});
+                afterstopword = true;
+            }
+            if(!afterstopword){
+                impl.gameevents.dialog_stream(chunk.data, 'character', {character: impl.chatting_with_villager});
+            }
+            session = session + chunk.data;
+        }
+    }
+
+    // detect any actions. They should be the end of the message, delimintated by '###'
+    // and written in JSON
+    console.log("Session: "+session);
+    let actions = session.split("###");
+    if(actions.length > 1){
+        // TODO! Try / catch block here
+        let act = JSON.parse(actions[actions.length - 1]);
+        console.log("Action: ");
+        console.log(act);
+        if(Object.prototype.hasOwnProperty.call(act, 'action')){
+            console.log("Dispatching action: "+act.action);
+            this.dispatch_action(act);
+        }else{
+            console.log("No action found in JSON object");
+        }
+    }
+
+    // TODO add to history
+    // this.history = this.history + "\n" + preamble + this.session;
+
+    if (!impl.is_chatting){
+      impl.gameevents.dialog_stream_done();
+    }
+}
--- src/games/penta/pentacity.js
+++ src/games/penta/pentacity.js
@@ -6,6 +6,7 @@ import * as VILLAGER  from './villager.js';
 import * as BT from '@spaced/bt.js';
 import * as GLOBALS from '@spaced/globals.js';
 import * as SCREEN from '@spaced/screen.js';
+import * as PENTA_STREAM from './penta_stream.js';
 
 import { sound } from '@pixi/sound';
 
@@ -15,9 +16,8 @@ const MAPFILE = import.meta.env.DEV
   ? '../games/penta/maps/penta.js'  // Dev path
   : '/maps/penta.js';  // Production path
 
+let impl = null;
 
-let chatting_with_villager = null;
-let shade_level = null;
 
 // Return static image object used by level.js to load images, size them, and create PIXI sprites from them 
 function static_images(){
@@ -52,24 +52,37 @@ var oneShotInit = (function() {
     };
 })();
 
-function initOnce(level){
-    let b = new BEING.Being();
+// -- 
+
+class PentaImpl{
+
+    constructor(gameevents){
+        this.gameevents = gameevents;
+        this.chatting_with_villager = null;
+        this.streaming = true;
+        this.shade_level = null;
+        this.is_chatting = false;
+        
+        // Use a rectangle with alpha to dim the level during dialogs
+        this.shade_level = new PIXI.Graphics();
+        this.shade_level.rect(0, 0, SCREEN.instance().width, SCREEN.instance().height);
+        this.shade_level.fill({color: 0x000000, alpha: 0.5});
+        this.shade_level.zIndex = GLOBALS.ZINDEX.DIALOG - 1;
+    }
+
 }
+
 
 // --
 // Start a conversation with the closest villager
 // --
 class EnterChatHandler {
 
-    constructor(gameevents){
-        this.gameevents = gameevents;
+    constructor(impl){
+        this.impl = impl;
+        this.gameevents = impl.gameevents;
         this.finished = false;
 
-        // Use a rectangle with alpha to dim the level during dialogs
-        shade_level = new PIXI.Graphics();
-        shade_level.rect(0, 0, SCREEN.instance().width, SCREEN.instance().height);
-        shade_level.fill({color: 0x000000, alpha: 0.5});
-        shade_level.zIndex = GLOBALS.ZINDEX.DIALOG - 1;
     }
 
     init(){
@@ -78,10 +91,11 @@ class EnterChatHandler {
         if(v){
             console.log("Talking to ", v.name);
             v.chatWithMainCharacter(this.gameevents.mainchar);
-            chatting_with_villager = v;
-            chatting_with_villager.container.zIndex = GLOBALS.ZINDEX.FOCUS;
+            impl.chatting_with_villager = v;
+            impl.chatting_with_villager.container.zIndex = GLOBALS.ZINDEX.FOCUS;
             this.gameevents.mainchar.stop();
             this.gameevents.mainchar.face('RIGHT');
+            this.impl.is_chatting = true;
         }else{
             console.log("No one close by");
         }
@@ -93,12 +107,12 @@ class EnterChatHandler {
 
     handle_bt_response(response){
         console.log("EnterChatHandler handle_bt_response", response);
-        chatting_with_villager.addToConversationHistory(chatting_with_villager.name, response);
-        this.gameevents.dialog_now(response, 'character', null, true, {character: chatting_with_villager});
+        impl.chatting_with_villager.addToConversationHistory(impl.chatting_with_villager.name, response);
+        this.gameevents.dialog_now(response, 'character', null, true, {character: impl.chatting_with_villager});
     }
 
     handle_input(input){
-        if(!chatting_with_villager){
+        if(!impl.chatting_with_villager){
             console.log("handle_input called after conversation ended. Bailing.");
             return;
         }
@@ -106,23 +120,33 @@ class EnterChatHandler {
             console.log("handle_input called with empty input. Bailing.");
             return;
         }
-        chatting_with_villager.addToConversationHistory(this.gameevents.mainchar.name, input);
+        impl.chatting_with_villager.addToConversationHistory(this.gameevents.mainchar.name, input);
 
-        BT.bt(chatting_with_villager.slug, {msg: input, history: chatting_with_villager.conversationHistoryAsText()}, this.handle_bt_response.bind(this));
+        const sysinput = {
+            history: impl.chatting_with_villager.conversationHistoryAsText(),
+            msg: input,
+        };
+        if (impl.streaming) {
+            PENTA_STREAM.invoke_prompt_input_stream(this.impl, impl.chatting_with_villager.slug, sysinput)
+        } else {
+            BT.bt(impl.chatting_with_villager.slug, sysinput, this.handle_bt_response.bind(this));
+        }
+        
     }
 
     finalize(){
         console.log("EnterChatHandler finalize");
-        this.gameevents.level.container.addChild(shade_level);
+        this.gameevents.level.container.addChild(impl.shade_level);
         this.gameevents.input_now("", this.handle_input.bind(this), {location: 'mainchar'});
-        this.gameevents.dialog_now("", 'character', null, true, {character: chatting_with_villager});
+        this.gameevents.dialog_now("", 'character', null, true, {character: impl.chatting_with_villager});
         this.finished = false;
     }
 }
 
 class LeaveChatHandler{
-    constructor(gameevents){
-        this.gameevents = gameevents;
+    constructor(impl){
+        this.impl = impl;
+        this.gameevents = impl.gameevents;
         this.finished = false;
     }
 
@@ -141,56 +165,57 @@ class LeaveChatHandler{
         console.log("LeaveChatHandler finalize");
         this.finished = false;
         this.gameevents.input_leave();
-        this.gameevents.level.container.removeChild(shade_level);
-        if(chatting_with_villager){
-            chatting_with_villager.endChatWithMainCharacter();
-            chatting_with_villager.container.zIndex = GLOBALS.ZINDEX.BEING;
+        this.gameevents.level.container.removeChild(impl.shade_level);
+        if(impl.chatting_with_villager){
+            impl.chatting_with_villager.endChatWithMainCharacter();
+            impl.chatting_with_villager.container.zIndex = GLOBALS.ZINDEX.BEING;
             this.gameevents.level.container.sortChildren();
-            chatting_with_villager = null;
+            impl.chatting_with_villager = null;
         }
 
         this.gameevents.clear_dialogs();
-        this.gameevents.register_key_handler("Enter", new EnterChatHandler(this.gameevents)); 
+        this.gameevents.register_key_handler("Enter", new EnterChatHandler(impl)); 
+        this.impl.is_chatting = false;
+        
     }
-
 }
 
 
-function init(gameevents) {
+PentaImpl.prototype.init = function(gameevents) {
 
-    oneShotInit(gameevents);
+    oneShotInit(this.gameevents);
 
     // Create a bunch of villagers 
-    let v = new VILLAGER.Villager("nancy", "nancy-first-27c7", gameevents.level.spritesheet_map.get("villager2"), gameevents.level);
-    gameevents.level.addBeing(v);
+    let v = new VILLAGER.Villager("nancy", "nancy-first-27c7", this.gameevents.level.spritesheet_map.get("villager2"), this.gameevents.level);
+    this.gameevents.level.addBeing(v);
     v.arrive(1000, 400);
 
-    let v2 = new VILLAGER.Villager("jane", "jane-first-a5e8", gameevents.level.spritesheet_map.get("villager3"), gameevents.level);
-    gameevents.level.addBeing(v2);
+    let v2 = new VILLAGER.Villager("jane", "jane-first-a5e8", this.gameevents.level.spritesheet_map.get("villager3"), this.gameevents.level);
+    this.gameevents.level.addBeing(v2);
     v2.arrive(800, 800);
 
-    let v3 = new VILLAGER.Villager("bob",  "bob-first-521e", gameevents.level.spritesheet_map.get("villager4"), gameevents.level);
-    gameevents.level.addBeing(v3);
+    let v3 = new VILLAGER.Villager("bob",  "bob-first-521e", this.gameevents.level.spritesheet_map.get("villager4"), this.gameevents.level);
+    this.gameevents.level.addBeing(v3);
     v3.arrive(400, 400);
 
-    let v4 = new VILLAGER.Villager("bill", "bill-first-3a38", gameevents.level.spritesheet_map.get("villager5"), gameevents.level);
-    gameevents.level.addBeing(v4);
+    let v4 = new VILLAGER.Villager("bill", "bill-first-3a38", this.gameevents.level.spritesheet_map.get("villager5"), this.gameevents.level);
+    this.gameevents.level.addBeing(v4);
     v4.arrive(400, 800);
 
-    let v5 = new VILLAGER.Villager("alice", "alice-first-218e", gameevents.level.spritesheet_map.get("villager6"), gameevents.level);
-    gameevents.level.addBeing(v5);
+    let v5 = new VILLAGER.Villager("alice", "alice-first-218e", this.gameevents.level.spritesheet_map.get("villager6"), this.gameevents.level);
+    this.gameevents.level.addBeing(v5);
     v5.arrive(800, 400);
 
-    let v6 = new VILLAGER.Villager("gordy", "gordy-first-16e8", gameevents.level.spritesheet_map.get("villager7"), gameevents.level);
-    gameevents.level.addBeing(v6);
+    let v6 = new VILLAGER.Villager("gordy", "gordy-first-16e8", this.gameevents.level.spritesheet_map.get("villager7"), this.gameevents.level);
+    this.gameevents.level.addBeing(v6);
     v6.arrive(1200, 400);
 
-    let v7 = new VILLAGER.Villager("jill", "jill-first-4720", gameevents.level.spritesheet_map.get("villager8"), gameevents.level);
-    gameevents.level.addBeing(v7);
+    let v7 = new VILLAGER.Villager("jill", "jill-first-4720", this.gameevents.level.spritesheet_map.get("villager8"), this.gameevents.level);
+    this.gameevents.level.addBeing(v7);
     v7.arrive(1200, 800);
 
-    gameevents.register_key_handler("Enter", new EnterChatHandler(gameevents));
-    gameevents.register_esc_handler(new LeaveChatHandler(gameevents));
+    this.gameevents.register_key_handler("Enter", new EnterChatHandler(this));
+    this.gameevents.register_esc_handler(new LeaveChatHandler(this));
 
     SCENE.setbgmusic('windandfire');
 } // init
@@ -214,11 +239,11 @@ class Penta extends LEVEL.Level {
     }
 
     initonce(){
-        return initOnce(this);
     }
 
     initonenter(ge){
-        return init(ge);
+        impl = new PentaImpl(ge);
+        return impl.init(ge);
     }
 }
 
